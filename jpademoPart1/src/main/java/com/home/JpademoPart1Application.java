364c3d6768ddc07cb30df98c066ce143b96d8d49
mohannautiyal <mohannautiyal80@gmail.com>
2019-09-20 20:36:20 +0530

    JPA concepts

---
+++ jpademoPart1/src/main/java/com/home/JpademoPart1Application.java
@@ -0,0 +1,13 @@
+package com.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpademoPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpademoPart1Application.class, args);
+	}
+
+}
