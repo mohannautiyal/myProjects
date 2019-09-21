7d597decbb41c43d93eb3f97714d4f656692d631
mohannautiyal <mohannautiyal80@gmail.com>
2019-09-21 09:30:29 +0530

    unit test case added

--- jpademoPart2/src/test/java/com/home/JpademoPart2ApplicationTests.java
+++ jpademoPart2/src/test/java/com/home/JpademoPart2ApplicationTests.java
@@ -1,16 +1,26 @@
 package com.home;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.home.repository.CourseRepository;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes=JpademoPart2Application.class)
 public class JpademoPart2ApplicationTests {
+	
+	@Autowired
+	CourseRepository courseRepo;
 
 	@Test
 	public void contextLoads() {
+		
+		assertTrue("Course Repository failed", courseRepo.findById(1001L).getName().equalsIgnoreCase("Spring Boot"));;
 	}
 
 }
